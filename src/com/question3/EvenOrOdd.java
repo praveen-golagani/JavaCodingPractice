a8d509e5a821b8b2b81cbb82c6c5e88c0f0888ed
praveen-golagani <praveenkqa800@gmail.com>
2025-11-13 22:02:11 +0530

    odd even

---
+++ src/com/question3/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package com.question3;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		int num = 15;
+		if((num & 1) ==0) {
+			System.out.println(num+" is Even.");
+		}else {
+			System.out.println(num+" is Odd.");
+		}
+
+	}
+
+}
